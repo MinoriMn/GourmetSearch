9731850bac64d016979cf19266e335a5608ce022
松田尚也 <minorim_n@M-Damatsumatsuda.local>
2022-04-09 21:40:11 +0900

    add: proj init

---
+++ GourmetSearchUITests/GourmetSearchUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GourmetSearchUITestsLaunchTests.swift
+//  GourmetSearchUITests
+//
+//  Created by 松田尚也 on 2022/04/09.
+//
+
+import XCTest
+
+class GourmetSearchUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
